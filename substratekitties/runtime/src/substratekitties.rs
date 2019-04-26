909226ac479db7ae13f16f31267e4ba9d07a4aa3
Weiliang Li <to.be.impressive@gmail.com>
2019-04-26 16:27:10 +0900

    [Add] event

---
+++ substratekitties/runtime/src/substratekitties.rs
@@ -0,0 +1,72 @@
+use parity_codec::{Decode, Encode};
+use runtime_primitives::traits::{As, Hash};
+use support::{
+    decl_event, decl_module, decl_storage, dispatch::Result, ensure, StorageMap, StorageValue,
+};
+use system::ensure_signed;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
+
+pub trait Trait: balances::Trait {
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_event!(
+    pub enum Event<T>
+    where
+        <T as system::Trait>::AccountId,
+        <T as system::Trait>::Hash
+    {
+        Created(AccountId, Hash),
+    }
+);
+
+decl_storage! {
+    trait Store for Module<T: Trait> as KittyStorage {
+        Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
+        OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;
+
+        Nonce: u64;
+    }
+}
+
+decl_module! {
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+        fn deposit_event<T>() = default;
+
+        fn create_kitty(origin) -> Result {
+            let sender = ensure_signed(origin).unwrap();
+            let nonce = <Nonce<T>>::get();
+            let seed = <system::Module<T>>::random_seed();
+            let random_hash = (seed, &sender, nonce).using_encoded(<T as system::Trait>::Hashing::hash);
+
+            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
+
+            let new_kitty = Kitty {
+                id: random_hash,
+                dna: random_hash,
+                price: <T::Balance as As<u64>>::sa(0),
+                gen: 0,
+            };
+
+            <Kitties<T>>::insert(random_hash, new_kitty);
+            <KittyOwner<T>>::insert(random_hash, &sender);
+            <OwnedKitty<T>>::insert(&sender, random_hash);
+
+            <Nonce<T>>::mutate(|n| *n += 1);
+
+            Self::deposit_event(RawEvent::Created(sender, random_hash));
+            Ok(())
+        }
+
+    }
+}
